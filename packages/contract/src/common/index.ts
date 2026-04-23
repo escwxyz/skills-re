bdf249ea6cf43ebcd27c27e3ab1eabd66922bf7a
Jie <linsijia628@gmail.com>
2026-04-23 12:47:24 +0200

    fix: type error

--- packages/contract/src/common/index.ts
+++ packages/contract/src/common/index.ts
@@ -1,9 +1,38 @@
 export * from "./base";
 export * from "./content";
-export * from "./enums";
+export { healthStatusSchema } from "./enums";
 export * from "./errors";
-export * from "./github";
+export {
+  fetchGithubRepoInputSchema,
+  fetchGithubRepoOutputSchema,
+  githubInvalidSkillPreviewSchema,
+  githubSkillPreviewSchema,
+  requestContextSchema,
+  submitGithubPreparedOutputSchema,
+  uploadRepoInputSchema,
+  uploadSkillInputSchema,
+} from "./github";
 export * from "./ids";
 export * from "./pagination";
-export * from "./slugs";
-export * from "./snapshots";
+export {
+  authorHandleInputSchema,
+  githubOwnerSchema,
+  githubRepoSchema,
+  skillSlugInputSchema,
+  skillSlugSchema,
+  tagSlugSchema,
+} from "./slugs";
+export {
+  fileContentSchema,
+  githubCommitSchema,
+  snapshotCreateHistoricalSnapshotsInputSchema,
+  snapshotDownloadEntrySchema,
+  snapshotFileEntrySchema,
+  snapshotFileManifestEntrySchema,
+  snapshotFileSignedUrlSchema,
+  snapshotHistoryCommitSchema,
+  snapshotItemSchema,
+  snapshotTreeEntrySchema,
+  snapshotUploadFileSchema,
+  treeEntrySchema,
+} from "./snapshots";
