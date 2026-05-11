12f646f2cfa136d169ef1e8643993a598e03d79e
Jie <linsijia628@gmail.com>
2026-05-11 12:15:32 +0200

    fix: format

--- apps/start/src/functions/authors/get-authors-list.ts
+++ apps/start/src/functions/authors/get-authors-list.ts
@@ -2,8 +2,24 @@ import { createServerFn } from "@tanstack/react-start";
 import { z } from "zod/v4";
 
 import { locales } from "@/paraglide/runtime";
-import { fetchAuthorsListPageData } from "./authors.server";
+import { buildAuthorsListPageData } from "@/utils/author-list-data";
+import { createServerORPCClient } from "@/lib/orpc.server";
 
+// todo: pagination
 export const getAuthorsList = createServerFn({ method: "GET" })
   .inputValidator(z.object({ locale: z.enum([...locales]) }))
-  .handler(({ data }) => fetchAuthorsListPageData(data.locale));
+  .handler(async ({ data }) => {
+    const client = createServerORPCClient();
+
+    const [authors, skillsCount] = await Promise.all([
+      client.skills.listAuthors(),
+      client.skills.count(),
+    ]);
+
+    return buildAuthorsListPageData({
+      authors,
+      dailyMetrics: await client.metrics.dailySkillsSnapshots({ limit: 7 }),
+      locale: data.locale,
+      skillsCount,
+    });
+  });
